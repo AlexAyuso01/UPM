9ff4b2bd4d1269ee40bb70f2bc9cba08638a08ad
AlexAyuso01 <alejandro.ayusoex@icloud.com>
2022-10-11 20:30:39 +0200

    INVERTIR TERMINADO

--- SEGUNDO/PPS/EJERCICIOS-C/pps/invertir.c
+++ SEGUNDO/PPS/EJERCICIOS-C/pps/invertir.c
@@ -7,7 +7,7 @@ int main(int argc, char ** argv){
     FILE *f;
     char espacio = ' ',a;
     int cadena1[M], cadena2[M], result[M];
-    int n = 0, ok = 0,cont = 0, i = 0, aux = 0, aux2 = 0, write = 0, wsnum = 0;
+    int n = 0, ok = 0,cont = 0, i = 0, aux = 0, aux2 = 0, write = 0, wsnum = 1;
     f = fopen("numeros.txt","r");
     if(f == NULL){
         printf("No such file\n");
@@ -19,9 +19,20 @@ int main(int argc, char ** argv){
         if(ok == 1){
             cont++;
             result[i]=cadena2[n];
+            //printf("%d\n",result[i]);
             i++;
-            wsnum ++;
+            if (feof(f)){
+                aux = i;
+                aux2 = aux;
+                while(aux > write){
+                    aux--;
+                    printf("%d ",result[aux]);
+                }
+                write = aux2;
+                printf("\n");
+            }
         } else { //leo algo que no sea un numero
+            wsnum = 0;
             if(cont != 0){
                 aux = i;
                 aux2 = aux;
@@ -38,11 +49,14 @@ int main(int argc, char ** argv){
         n++;
         
     }
-    if (wsnum == 0){
-        while(i >= 0){
-            printf("%d ",result[i]);
+    if (wsnum != 0){
+        printf("%d\n",wsnum);
+        printf("hola me he colado\n");
+        while(i > 0){
             i--;
+            printf("%d ",result[i]);
         }
+        printf("\n");
     }
     fclose(f);
     return(0);
