5ed4cc2bae51ed7c2107871a3afe261152700946
AlexAyuso01 <alejandro.ayusoex@icloud.com>
2022-10-11 17:53:31 +0200

    invertir hecho

--- SEGUNDO/PPS/EJERCICIOS-C/pps/invertir.c
+++ SEGUNDO/PPS/EJERCICIOS-C/pps/invertir.c
@@ -6,39 +6,36 @@
 int main(int argc, char ** argv){
     FILE *f;
     char espacio = ' ',a;
-    int cadena1[M], cadena2[M],err[M] ;
+    int cadena1[M], cadena2[M], result[M];
+    int n = 0, ok = 0,cont = 0, i = 0, aux = 0, aux2 = 0, write = 0;
     f = fopen("numeros.txt","r");
     if(f == NULL){
         printf("No such file\n");
         exit(0);
     }
-    int n = 0;
-    int ok = 0;
-    //printf("%d\n",n);
     while(!feof(f)){
         ok = fscanf(f,"%d",&cadena1[n]);
-        cadena2[n] = cadena1[n];   
+        cadena2[n] = cadena1[n];
+        if(ok == 1){
+            cont++;
+            result[i]=cadena2[n];
+            i++;
+        } else {
+            if(cont != 0){
+                aux = i;
+                aux2 = aux;
+                while(aux > write){
+                    aux--;
+                    printf("%d ",result[aux]);
+                }
+                write = aux2;
+                printf("\n");
+                cont = 0;
+            }  
+        }             
         fgetc(f);
         n++;
-    }
-    //printf("%d",cadena2[0]);
-    int m = n-1;
-    int j = 0;
-    int nzeros = 0;
-    while(m >= j){
-        if (cadena2[j] == 0 && nzeros == 0){
-            printf("\n");
-            j++; 
-            nzeros++;
-        }
-        else if (nzeros == 1 && cadena1[j+1] != 0){
-            nzeros = 0;   
-        } else if(cadena2[j] == 0){
-            j++;
-        } else {
-            printf("%d ",cadena2[j]);
-            j++;
-        }
+        
     }
     fclose(f);
     return(0);
