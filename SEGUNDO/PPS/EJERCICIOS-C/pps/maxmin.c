d337f6b9d798777325780f1a77fa6d82edd07a64
Alejandro  Ayuso Exposito <alejandro.ayusoex@alumnos.upm.es>
2022-11-28 11:54:24 +0100

    EJ 2.1 PPS OK

---
+++ SEGUNDO/PPS/EJERCICIOS-C/pps/maxmin.c
@@ -0,0 +1,39 @@
+//program that reads a file and checks what are the maximum and minimum values of the numbers in the file
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv){
+    FILE *f;
+    float num;
+    float max = 0.00;
+    float min = 0.00;
+    int var = 0;
+    if (argc != 2){
+        printf("Error: wrong number of arguments\n");
+        var = -1;
+    } else if (fopen(argv[1], "r") == NULL){
+        printf("Error opening file\n");
+        var = -1;
+    } else if (fscanf(f, "%f", &num) == EOF){
+        printf("%10.2f %10.2f\n", max, min);
+        var = 0;
+    } else {
+        fseek(f, 0, SEEK_SET);
+        fscanf(f, "%f", &num);
+        max = num;
+        min = num;
+        while (fscanf(f, "%f", &num) != EOF){
+                
+            if (num > max){
+                max = num;
+            }
+            if (num < min){
+                min = num;
+            }
+
+        }      
+        var = 0;
+        printf("%10.2f %10.2f\n", max, min);
+    }
+    return var;
+}
