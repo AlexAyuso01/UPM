f9f60fdb4960adc94720332bd89a299aceb70102
Alejandro Ayuso Exposito <83668978+AlexAyuso01@users.noreply.github.com>
2023-04-17 21:10:45 +0200

    cells/3 casi terminado
    fallan 4 de 64 tests, falta arreglar el caso principal, que empiece y acabe por celula [o,...,o]

--- TERCERO/PROLOG/PRACTICA_1/code.pl
+++ TERCERO/PROLOG/PRACTICA_1/code.pl
@@ -1,11 +1,10 @@
 :- module(_,_,[]).
+
 author_data('Ayuso', 'Exposito', 'Alejandro', '190238').
 
-%color
 color(o).
 color(x).
 
-%rule/3
 rule(o,o,o,_,o). % regla nula
 rule(x,o,o,r(A,_,_,_,_,_,_),A) :- color(A).
 rule(o,x,o,r(_,B,_,_,_,_,_),B) :- color(B).
@@ -14,59 +13,30 @@ rule(x,o,x,r(_,_,_,D,_,_,_),D) :- color(D).
 rule(x,x,o,r(_,_,_,_,E,_,_),E) :- color(E).
 rule(o,x,x,r(_,_,_,_,_,F,_),F) :- color(F).
 rule(x,x,x,r(_,_,_,_,_,_,G),G) :- color(G).
+% Células en un estado inicial dado
+% Caso base: un solo elemento en la lista
+cells([o], _, [o,o,o]).
+% Caso dos elementos en la lista
+cells([C1,C2], R, [o,NewC1,NewC2,o]) :-
+    rule(C1, C2, o, R, NewC1),
+    rule(C2, o, o, R, NewC2).
+% Caso recursivo: expandir el estado inicial y aplicar reglas
+% Células en un estado inicial dado
+% Agregar dos células blancas al principio y al final del estado
+% Aplicar reglas a cada conjunto de 3 células consecutivas
+cells(State, R, Result) :-
+    my_append([o,o], State, PaddedState),
+    my_append(PaddedState, [o,o], PaddedResult),
+    apply_rules(PaddedResult, R, Result), !.
 
-% my_reverse/3
-my_reverse(Xs, Ys) :- my_reverse(Xs, [], Ys).
+% Aplica la regla correspondiente a cada conjunto de 3 células consecutivas
+apply_rules([C1,C2,C3 | Rest], R, [NewC1 | NewRest]) :-
+    rule(C1, C2, C3, R, NewC1),
+    apply_rules([C2,C3 | Rest], R, NewRest).
+apply_rules(_, _, []). % Caso base: no hay más células
 
-my_reverse([], Acc, Acc).
-my_reverse([X|Xs], Acc, Ys) :- my_reverse(Xs, [X|Acc], Ys).
-
-% my_length/3
-my_length(Xs, N) :- my_length(Xs, 0, N).
-
-my_length([], N, N).
-my_length([_|Xs], Acc, N) :- my_length(Xs, s(Acc), N).
-
-% my_append/3
-my_append([], Ys, Ys).
-my_append([X|Xs], Ys, [X|Zs]) :- my_append(Xs, Ys, Zs).
-
-% apply_rules/3
-apply_rules([], _, []).
-apply_rules([A,B,C|StateIn], Rules, [X|StateOut]) :-
-    apply_rule(A,B,C,Rules,X),
-    apply_rules([B,C|StateIn], Rules, StateOut).
-
-% apply_rule/5
-apply_rule(A, B, C, Rules, X) :-
-    rule(A,B,C,Rules,X),
-    color(X).
-
-% first_last_cells/3
-first_last_cells(State, First, Last) :- State = [First|_], my_reverse(State, [Last|_]).
-
-% check_valid_state/1
-check_valid_state(State) :-
-    my_length(State, Len),
-    my_length(s(s(o)), MinLen),
-    len_ge(Len, MinLen),
-    first_last_cells(State, First, Last),
-    First = o, Last = o.
-
-% len_ge/2 (helper function to check if Len >= MinLen using Peano arithmetic)
-len_ge(X, Y) :- len_ge(X, Y, o).
-
-len_ge(X, X, _).
-len_ge(s(X), Y, s(Z)) :- len_ge(X, Y, Z).
-
-% cells/3
-cells(StateIn, Rules, StateOut) :-
-    check_valid_state(StateIn),
-    apply_rules(StateIn, Rules, StateOut0),
-    my_append(StateOut0, [o,o], StateOut),
-    check_valid_state(StateOut),
-    StateOut = [_|StateOut1],
-    StateOut1 = [o|_],
-    !.
+% Agregar elementos al inicio y al final de la lista
+my_append([], L, L).
+my_append([H|T], L, [H|Result]) :- my_append(T, L, Result).
 
 
