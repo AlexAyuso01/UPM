a2dd8ba96c3db0f8006ba38910a846c1500009d5
AYUSO EXPÓSITO, ALEJANDRO <alejandro.ayusoex@alumnos.upm.es>
2023-05-10 18:14:36 +0200

    minishell rebuild

--- TERCERO/SSOO/DATSI/so/msh.2022b/main.c
+++ TERCERO/SSOO/DATSI/so/msh.2022b/main.c
@@ -1,131 +1,137 @@
-/*-
- * main.c
- * Minishell C source
- * Shows how to use "obtain_order" input interface function.
- *
- * Copyright (c) 1993-2002-2019, Francisco Rosales <frosal@fi.upm.es>
- * Todos los derechos reservados.
- *
- * Publicado bajo Licencia de Proyecto Educativo Práctico
- * <http://laurel.datsi.fi.upm.es/~ssoo/LICENCIA/LPEP>
- *
- * Queda prohibida la difusión total o parcial por cualquier
- * medio del material entregado al alumno para la realización
- * de este proyecto o de cualquier material derivado de este,
- * incluyendo la solución particular que desarrolle el alumno.
- *
- * DO NOT MODIFY ANYTHING OVER THIS LINE
- * THIS FILE IS TO BE MODIFIED
- */
-
-#include <stddef.h>			/* NULL */
-#include <stdio.h>			/* setbuf, printf */
+#include <stddef.h>     /* NULL */
+#include <stdio.h>      /* setbuf, printf */
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <unistd.h>
 #include <string.h>
-#include <limits.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-extern int obtain_order();		/* See parser.y for description */
-int esmandatointerno(int argc, char **argv);
-void getCurrentDirectory(int argc, char **argv);
+#define MAX_LINE 80 /* The maximum length of a command */
+
+extern int obtain_order();     /* See parser.y for description */
+void execute_pipeline(char ***argvv);
+void handle_redirections(char *filev[], char **in_file, char **out_file);
 
 int main(void)
 {
-	char ***argvv = NULL;
-	int argvc;
-	char **argv = NULL;
-	int argc;
-	char *filev[3] = { NULL, NULL, NULL };
-	int bg;
-	int ret;
+    char ***argvv = NULL;
+    int argvc;
+    //char **argv = NULL;
+    char *filev[3] = {NULL, NULL, NULL};
+    int bg;
+    int ret;
+	char *in_file = NULL;
+	char *out_file = NULL;
+	
+    setbuf(stdout, NULL); /* Unbuffered */
+    setbuf(stdin, NULL);
 
-	setbuf(stdout, NULL);			/* Unbuffered */
-	setbuf(stdin, NULL);
-
-	while (1) {
-		fprintf(stderr, "%s", "msh> ");	/* Prompt */
-		ret = obtain_order(&argvv, filev, &bg);
-		if (ret == 0) break;		/* EOF */
-		if (ret == -1) continue;	/* Syntax error */
-		argvc = ret - 1;		/* Line */
-		if (argvc == 0) continue;	/* Empty line */
-		
-#if 1
-/*
- * LAS LINEAS QUE A CONTINUACION SE PRESENTAN SON SOLO
- * PARA DAR UNA IDEA DE COMO UTILIZAR LAS ESTRUCTURAS
- * argvv Y filev. ESTAS LINEAS DEBERAN SER ELIMINADAS.
- */
-		for (argvc = 0; (argv = argvv[argvc]); argvc++) {
-			for (argc = 0; argv[argc]; argc++){
-				if (argv[argc] != NULL){
-					if (esmandatointerno(argc, argv) != 0){
-						wait(NULL);
-					} else {
-						int pid = fork();
-						if (pid == 0){
-							execvp(argv[0], argv);
-						} else{
-							wait(NULL);
-						}
-					}
-				}
-			}
-		}
-		if (filev[0]) printf("< %s\n", filev[0]);/* IN */
-		if (filev[1]) printf("> %s\n", filev[1]);/* OUT */
-		if (filev[2]) printf(">& %s\n", filev[2]);/* ERR */
-		if (bg) printf("&\n");
-/*
- * FIN DE LA PARTE A ELIMINAR
- */
-#endif
-	}
-	exit(0);
-	return 0;
-}
-// check if the command is an internal command
-int esmandatointerno(int argc, char **argv){ // NO FUNCIONA AUN PERO PORQUE NO SE ACTUALIZA EL CURRENT DIRECTORY
-	//checks if it is the cd unix command, if it is, it changes the directory
-	if (strcmp(argv[0], "cd") == 0){
-		if (argc == 0){ // if there is no argument, it changes to the home directory
-			chdir(getenv("HOME"));
-		} else if (argc == 1){ // if there is one argument, it changes to the directory specified
-			getCurrentDirectory(argc, argv);
-		} else { // if there are more than one argument, it prints an error
-			printf("cd: too many arguments\n");
-		}
-		return 1;
-	} else if (strcmp(argv[0], "exit") == 0){
-		exit(0);
-	} else if (strcmp(argv[0], "umask") == 0){
-		printf("umask: not implemented\n");
-		return 1;
-	} else if (strcmp(argv[0], "limit") == 0){
-		return 1;
+    while (1)
+    {
+        fprintf(stderr, "%s", "msh> "); /* Prompt */
+        ret = obtain_order(&argvv, filev, &bg);
+        if (ret == 0)
+            break;     /* EOF */
+        if (ret == -1)
+            continue; /* Syntax error */
+        argvc = ret - 1; /* Line */
+        if (argvc == 0)
+            continue; /* Empty line */
+		handle_redirections(filev, &in_file, &out_file);
+		execute_pipeline(argvv);
 	}
 	return 0;
 }
-//function that gets the current directory and changes it to the specified directory
-void getCurrentDirectory(int argc, char **argv){
-	//get the current directory and store it in dir 
-	//char dir[PATH_MAX];
-	//getcwd(dir,sizeof(dir));
-	//char *cwd = "${workspaceFolder}";
-	//char *cwd = getcwd(NULL,0);
-	//concatenate the current directory with the argument
-	char cwd [1024];
-	if (getcwd(cwd, sizeof(cwd)) != NULL){
-		printf("%s\n", cwd);
-		strcat(cwd, "/");
-		printf("%s\n",cwd);
-		strcat(cwd, argv[1]);
-		//change the directory to dir
-		chdir(cwd);
-		printf("%s\n", cwd);
-		printf("%d\n", argc);
-		//free(dir);
+
+void execute_pipeline(char ***argvv)
+{
+    int num_commands = 0;
+    for (int i = 0; argvv[i] != NULL; i++) {
+        num_commands++;
+    }
+
+    int pipes[num_commands - 1][2];
+    for (int i = 0; i < num_commands - 1; i++) {
+        if (pipe(pipes[i]) == -1) {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (int i = 0; i < num_commands; i++) {
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            if (i == 0 && num_commands > 1) {
+                // First command in pipeline
+                dup2(pipes[i][1], STDOUT_FILENO);
+                close(pipes[i][0]);
+                close(pipes[i][1]);
+            } else if (i == num_commands - 1 && num_commands > 1) {
+                // Last command in pipeline
+                dup2(pipes[i - 1][0], STDIN_FILENO);
+                close(pipes[i - 1][0]);
+                close(pipes[i - 1][1]);
+            } else if (num_commands > 1) {
+                // Command in the middle of the pipeline
+                dup2(pipes[i - 1][0], STDIN_FILENO);
+                close(pipes[i - 1][0]);
+                close(pipes[i - 1][1]);
+                dup2(pipes[i][1], STDOUT_FILENO);
+                close(pipes[i][0]);
+                close(pipes[i][1]);
+            }
+
+            if (execvp(argvv[i][0], argvv[i]) == -1) {
+                perror("execvp");
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            if (i == 0 && num_commands > 1) {
+                close(pipes[i][1]);
+            } else if (i == num_commands - 1 && num_commands > 1) {
+                close(pipes[i - 1][0]);
+            } else if (num_commands > 1) {
+                close(pipes[i - 1][0]);
+                close(pipes[i][1]);
+            }
+		}
+	}
+
+	for (int i = 0; i < num_commands; i++) {
+    	wait(NULL);
 	}
 }
+
+void handle_redirections(char *filev[], char **in_file, char **out_file) {
+    if (filev[0] != NULL) {
+        *in_file = filev[0];
+    }
+    if (filev[1] != NULL) {
+        *out_file = filev[1];
+    }
+    
+    if (*in_file != NULL) {
+        int fd = open(*in_file, O_RDONLY);
+        if (fd == -1) {
+            perror("open");
+            exit(EXIT_FAILURE);
+        }
+        dup2(fd, STDIN_FILENO);
+        close(fd);
+    }
+    
+    if (*out_file != NULL) {
+        int fd = open(*out_file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+        if (fd == -1) {
+            perror("open");
+            exit(EXIT_FAILURE);
+        }
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
+    }
+}
+
+
